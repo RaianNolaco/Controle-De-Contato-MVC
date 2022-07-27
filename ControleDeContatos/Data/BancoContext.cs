7608d75107779605a53c6bb0464b6ed107f3ba71
Raian Medeiros <raiannolaco@gmail.com>
2022-07-26 23:37:53 -0300

    Criando Model e configurando Entity Framework

---
+++ ControleDeContatos/Data/BancoContext.cs
@@ -0,0 +1,11 @@
+﻿using ControleDeContatos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ControleDeContatos.Data
+{
+    public class BancoContext : DbContext
+    {
+        public BancoContext(DbContextOptions<BancoContext> options) : base(options){}
+        public DbSet<ContatoModel> Contatos { get; set; }
+    }
+}
---
+++ ControleDeContatos/Models/ContatoModel.cs
@@ -0,0 +1,10 @@
+﻿namespace ControleDeContatos.Models
+{
+    public class ContatoModel
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Numero { get; set; }
+    }
+}
