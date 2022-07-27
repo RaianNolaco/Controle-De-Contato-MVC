a13a376de8bcefb28f97ed8fbe1babf070b2fa8b
Raian Medeiros <raiannolaco@gmail.com>
2022-07-27 02:11:54 -0300

    Configurando connection string e banco de dados

--- ControleDeContatos/Startup.cs
+++ ControleDeContatos/Startup.cs
@@ -1,5 +1,7 @@
+using ControleDeContatos.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -23,6 +25,9 @@ namespace ControleDeContatos
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            //Configurando Entity Framework para conectar com Sql server
+            services.AddEntityFrameworkSqlServer()
+                .AddDbContext<BancoContext>(o => o.UseSqlServer(Configuration.GetConnectionString("DataBase")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
